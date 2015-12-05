d0f5b27d7f1cd2453229952ddd3dea88dc8a6c24
dvladimirovich <byrmistrov89@gmail.com>
2015-12-05 23:01:04 +0200

    Создан класс, который будет использоваться для обмена даными между формами.

---
+++ GuessTheMelody/GuessTheMelody/Victorina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheMelody
+{
+    /// <summary>
+    /// Класс для передачи данных между формами
+    /// </summary>
+    internal static class Victorina
+    {
+        static public List<string> List = new List<string>(); 
+    }
+}
