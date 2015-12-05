7f203ebfca9933aba1b206230b33685fc2b0e86e
dvladimirovich <byrmistrov89@gmail.com>
2015-12-06 01:10:42 +0200

    - Добавили параметры настройки. - Создали методы чтения и сохранения настроек в реестр

--- GuessTheMelody/GuessTheMelody/Victorina.cs
+++ GuessTheMelody/GuessTheMelody/Victorina.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace GuessTheMelody
 {
@@ -11,6 +13,87 @@ namespace GuessTheMelody
     /// </summary>
     internal static class Victorina
     {
-        static public List<string> List = new List<string>(); 
+        /// <summary>
+        /// Список музыкальных композиций
+        /// </summary>
+        static public List<string> TrackList = new List<string>();
+        /// <summary>
+        /// Продолжительность игры. Значение по умолчанию - 60 секунд
+        /// </summary>
+        public static int GameDuration = 60;
+        /// <summary>
+        /// Продолжительность проигрывания трека. Значение по умолчанию - 10 секунд
+        /// </summary>
+        public static int MusicDuration = 10;
+        /// <summary>
+        /// Начинать проигрывать композицию сначала или со случайного места. Значение по умолчанию - сначала
+        /// </summary>
+        public static bool RandomStart = false;
+        /// <summary>
+        /// Путь к последней выбранной папке.
+        /// </summary>
+        public static string LastFolder = string.Empty;
+        /// <summary>
+        /// Включать ли вложенные каталоги при загрузке треков. Значение по умолчанию - не включать
+        /// </summary>
+        public static bool AllDirectories = false;
+        
+        /// <summary>
+        /// Считать музыкальные композиции
+        /// </summary>
+        static public void ReadMusic()
+        {
+            string[] musicArray = Directory.GetFiles(LastFolder, "*.mp3", AllDirectories
+                                                                              ? SearchOption.AllDirectories
+                                                                              : SearchOption.TopDirectoryOnly);
+            TrackList.Clear();
+            TrackList.AddRange(musicArray);
+        }
+
+        private static string regKeyName = @"Software\MyCompanyName\GuessTheMelody";
+
+        /// <summary>
+        /// Сохраняет настройки приложения в реестр
+        /// </summary>
+        public static void SaveParam()
+        {
+            RegistryKey regkey = null;
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(regKeyName);
+                if (regkey == null) return;
+                regkey.SetValue("LastFolder", LastFolder);
+                regkey.SetValue("AllDirectories", AllDirectories);
+                regkey.SetValue("RandomStart", RandomStart);
+                regkey.SetValue("GameDuration", GameDuration);
+                regkey.SetValue("MusicDuration", MusicDuration);
+            }
+            finally 
+            {
+                if (regkey != null) regkey.Close();
+            }
+        }
+
+        /// <summary>
+        /// Считывает настройки приложения из реестра
+        /// </summary>
+        public static void ReadParam()
+        {
+            RegistryKey regkey = null;
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(regKeyName);
+                if (regkey == null) return;
+                LastFolder = (string) regkey.GetValue("LastFolder", "");
+                AllDirectories = Convert.ToBoolean(regkey.GetValue("AllDirectories", false));
+                RandomStart = Convert.ToBoolean(regkey.GetValue("RandomStart", false));
+                GameDuration = (int) regkey.GetValue("GameDuration", 60);
+                MusicDuration = (int) regkey.GetValue("MusicDuration", 10);
+            }
+            finally
+            {
+                if (regkey != null) regkey.Close();
+            }
+        }
     }
 }
