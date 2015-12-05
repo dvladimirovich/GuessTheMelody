847a90df8fa11d91a884c9aa2c8251ecec3c8650
dvladimirovich <byrmistrov89@gmail.com>
2015-12-05 22:42:30 +0200

    - Создали форму с параметрами. - Разместили компоненты. - Добавили возможность добавления списка песен, в зависимости от свойства "Вложенные папки"

---
+++ GuessTheMelody/GuessTheMelody/FrmParams.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GuessTheMelody
+{
+    partial class FrmParams
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMusicTrack = new System.Windows.Forms.ListBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.chkbAllDirectories = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbMusicTrack
+            // 
+            this.lbMusicTrack.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbMusicTrack.FormattingEnabled = true;
+            this.lbMusicTrack.Location = new System.Drawing.Point(0, 0);
+            this.lbMusicTrack.Name = "lbMusicTrack";
+            this.lbMusicTrack.Size = new System.Drawing.Size(421, 186);
+            this.lbMusicTrack.TabIndex = 0;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 193);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(104, 23);
+            this.btnSelectFolder.TabIndex = 1;
+            this.btnSelectFolder.Text = "Выбрать папку";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Location = new System.Drawing.Point(122, 193);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(75, 23);
+            this.btnClearList.TabIndex = 1;
+            this.btnClearList.Text = "Очистить";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            // 
+            // chkbAllDirectories
+            // 
+            this.chkbAllDirectories.AutoSize = true;
+            this.chkbAllDirectories.Location = new System.Drawing.Point(291, 197);
+            this.chkbAllDirectories.Name = "chkbAllDirectories";
+            this.chkbAllDirectories.Size = new System.Drawing.Size(118, 17);
+            this.chkbAllDirectories.TabIndex = 2;
+            this.chkbAllDirectories.Text = "Вложенные папки";
+            this.chkbAllDirectories.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(334, 313);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(253, 313);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // FrmParams
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(421, 348);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.chkbAllDirectories);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.lbMusicTrack);
+            this.Name = "FrmParams";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Настройки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMusicTrack;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.CheckBox chkbAllDirectories;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ GuessTheMelody/GuessTheMelody/FrmParams.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GuessTheMelody
+{
+    public partial class FrmParams : Form
+    {
+        public FrmParams()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] musicArray = Directory.GetFiles(fbd.SelectedPath, "*.mp3",
+                                                             chkbAllDirectories.Checked
+                                                                 ? SearchOption.AllDirectories
+                                                                 : SearchOption.TopDirectoryOnly);
+                    lbMusicTrack.Items.Clear();
+                    lbMusicTrack.Items.AddRange(musicArray);
+                }
+            }
+        }
+    }
+}
