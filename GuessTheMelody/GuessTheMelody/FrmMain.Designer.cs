51914273cf138a1a740c1020a520c7d13740c803
dvladimirovich <byrmistrov89@gmail.com>
2015-12-05 22:09:54 +0200

    - Создали новое приложение типа Windows Forms. - Добавили иконку для формы. - Добавили изображения фона и установили одно из них активным. - Добавили кнопки главного меню и обработали нажатие на кнопку "Выход".

---
+++ GuessTheMelody/GuessTheMelody/FrmMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GuessTheMelody
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnParams = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlay.Location = new System.Drawing.Point(75, 36);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(226, 51);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Игра";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnParams
+            // 
+            this.btnParams.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnParams.Location = new System.Drawing.Point(75, 93);
+            this.btnParams.Name = "btnParams";
+            this.btnParams.Size = new System.Drawing.Size(226, 51);
+            this.btnParams.TabIndex = 0;
+            this.btnParams.Text = "Настройки";
+            this.btnParams.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(75, 150);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(226, 51);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GuessTheMelody.Properties.Resources.music_2;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(364, 345);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnParams);
+            this.Controls.Add(this.btnPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(380, 383);
+            this.MinimumSize = new System.Drawing.Size(380, 383);
+            this.Name = "FrmMain";
+            this.Text = "Угадай мелодию";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnParams;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ GuessTheMelody/GuessTheMelody/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Угадай Мелодию")]
+[assembly: AssemblyDescription("Игра для двух человек")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuessTheMelody")]
+[assembly: AssemblyCopyright("Copyright © Burmistrov D.V.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("26d6780a-355f-4a53-9949-c0c9a7987860")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
